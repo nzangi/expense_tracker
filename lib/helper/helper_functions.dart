a185c753219e711fe2cd4604b78231cea7e58524
nzangi <nzangimuoki284@gmail.com>
2024-08-01 00:10:51 +0300

    Done on UI CRUD

--- lib/helper/helper_functions.dart
+++ lib/helper/helper_functions.dart
@@ -2,7 +2,14 @@
  These are some of the helper functions
  */
 
+import 'package:intl/intl.dart';
+
 double convertStringToDouble(String string){
   double? amount = double.tryParse(string);
   return amount ?? 0 ;
+}
+String formatAmount(double amount){
+  final format = NumberFormat.currency(locale: "en_US",symbol: "KSH ",decimalDigits: 2);
+  return format.format(amount);
+
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../components/my_list_tile.dart';
 import '../database/expense_database.dart';
 import '../helper/helper_functions.dart';
 
@@ -36,14 +37,69 @@ class _HomePageState extends State<HomePage> {
                   itemBuilder: (context, index) {
                     // get individual expense
                     Expense individualExpense = value.allExpenses[index];
-                    return ListTile(
-                      title: Text(individualExpense.name),
-                      trailing: Text(individualExpense.amount.toString()),
+                    return MyListTile(
+                      title: individualExpense.name,
+                      trailing: formatAmount(individualExpense.amount),
+                      onDeletePressed: (context) =>
+                          openDeleteBox(individualExpense),
+                      onEditPressed: (context) =>
+                          openEditBox(individualExpense),
                     );
                   }),
             ));
   }
 
+  // delete button
+  void openDeleteBox(Expense expense) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Delete Expense'),
+        actions: [
+          //   cancel button
+          _cancelButton(),
+
+          //   Delete button
+          _deleteExpenseButton(expense.id)
+        ],
+      ),
+    );
+  }
+
+  void openEditBox(Expense expense) {
+    String existingName = expense.name;
+    String existingAmount = expense.amount.toString();
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Edit Expense'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            //   user input, expense name
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(hintText: existingName),
+            ),
+            TextField(
+              controller: amountController,
+              decoration: InputDecoration(hintText: existingAmount),
+            ),
+          ],
+        ),
+        actions: [
+          //   cancel button
+          _cancelButton(),
+
+          //   save button
+          _editExpenseButton(expense)
+        ],
+      ),
+    );
+  }
+
+  // expense box
   void openExpenseBox() {
     showDialog(
         context: context,
@@ -73,6 +129,7 @@ class _HomePageState extends State<HomePage> {
             ));
   }
 
+  // cancel button
   Widget _cancelButton() {
     return MaterialButton(
       onPressed: () {
@@ -86,6 +143,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  // create expense
   Widget _createExpense() {
     return MaterialButton(
       onPressed: () async {
@@ -107,6 +165,49 @@ class _HomePageState extends State<HomePage> {
         }
       },
       child: const Text('Save'),
+    );
+  }
+
+  // save update
+  Widget _editExpenseButton(Expense expense) {
+    return MaterialButton(
+      onPressed: () async {
+        //   save if name or amount has changed
+        if (nameController.text.isNotEmpty ||
+            amountController.text.isNotEmpty) {
+          //   pop box
+          Navigator.pop(context);
+          //   create new updated expense
+          Expense updatedExpense = Expense(
+              name: nameController.text.isNotEmpty
+                  ? nameController.text
+                  : expense.name,
+              amount: amountController.text.isNotEmpty
+                  ? convertStringToDouble(amountController.text)
+                  : expense.amount,
+              date: DateTime.now());
+          //   old expense id
+          int existingId = expense.id;
+
+          //   update
+          await context
+              .read<ExpenseDatabase>()
+              .updateExpense(existingId, updatedExpense);
+        }
+      },
+      child: const Text('Update'),
+    );
+  }
+
+  Widget _deleteExpenseButton(int id) {
+    return MaterialButton(
+      onPressed: () async {
+        //   pop box
+        Navigator.pop(context);
+        //   delete from expense db
+        await context.read<ExpenseDatabase>().deleteExpense(id);
+      },
+      child: const Text('Delete'),
     );
   }
 }
